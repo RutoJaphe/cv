bb4f68811484b11b91ffe96cdf7a56f15818f439
Japheth Rutoh <jruhtz@gmail.com>
2024-01-24 13:32:39 +0300

    fixed issue with page.tsx personalWebsiteUrl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,18 +185,7 @@ export default function Page() {
         </Section>
       </section>
 
-      <CommandMenu
-        links={[
-          // {
-          //   url: RESUME_DATA.personalWebsiteUrl,
-          //   title: "Personal Website",
-          // },
-          ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
-            url: socialMediaLink.url,
-            title: socialMediaLink.name,
-          })),
-        ]}
-      />
+  
     </main>
   );
 }
